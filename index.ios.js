9def2c408d7ed2f23430ffc7d125fd7e7c140b79
Fengwei Zhang <jerry.fengwei@gmail.com>
2017-05-18 11:50:09 -0400

    UI change for evenly distribute elements

--- index.ios.js
+++ index.ios.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View, StyleSheet, TouchableWithoutFeedback, Keyboard } from 'react-native';
 class Calculator extends Component {
   constructor(props) {
     super(props);
@@ -9,33 +9,64 @@ class Calculator extends Component {
 
   render() {
     return (
-      <View style={{padding: 24}}>
-        <Text style={{fontSize: 18}}>Bill Amount:</Text>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+
+        <View>
+        <View style = {styles.card}>
+        
         <TextInput
-          style={{height: 40}}
+          style={styles.input}
           placeholder="Enter Bill Amount"
           keyboardType = 'numeric'
           onChangeText={(amount) => this.setState({amount})}
-        />
-        <Text style={{fontSize: 18}}>Tip Percentage:</Text>
+        /><Text style={styles.title}>After Tax</Text>
+        </View>
+         <View style = {styles.card}>
+        
         <TextInput
-          style={{height: 40}}
+          style={styles.input}
           placeholder="Enter Tip Percentage"
           keyboardType = 'numeric'
           onChangeText={(percentage) => this.setState({percentage})}
         />
-        <Text style={{fontSize: 18}}>Number of people:</Text>
+        <Text style={styles.title}>%</Text>
+       </View>
+         <View style = {styles.card}>
+        
         <TextInput
-          style={{height: 40}}
+          style={styles.input}
           placeholder="Enter Number of people"
           keyboardType = 'numeric'
           onChangeText={(people) => this.setState({people})}
         />
-
-            <Text style={{fontSize: 24}}>Each Person:</Text>
-                    <Text>{this.state.text.split(' ').map((word) => (this.state.amount * (1 + this.state.percentage/100))/this.state.people).join(' ')}</Text></View>
+        <Text style={styles.title}>Number of people:</Text>
+</View>
+        <View style = {styles.card}>
+                    <Text style={styles.input} >{this.state.text.split(' ').map((word) => (this.state.amount * (1 + this.state.percentage/100))/this.state.people).join(' ')}</Text><Text style={styles.title}>Per Person</Text></View>
+                        </View></TouchableWithoutFeedback>
 );
 }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    padding: 12,
+    textAlign:'right', color: 'white',
+  },
+  input: {
+    color: '#dddddd',
+   padding: 18,
+      flex:3,
+
+  },
+    card: {
+    backgroundColor: '#149688',
+      borderColor: 'white',
+    shadowColor:'black',  
+        margin: 36
+  }
+});
 
 AppRegistry.registerComponent('cal', () => Calculator);
